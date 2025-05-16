1998cb10d320c4adc323107032cfd20b52da7dd2
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-05-16 17:43:17 -0300

    feat: Added the counter example with redux. Finished the section and the course!

--- redux/src/index.js
+++ redux/src/index.js
@@ -4,14 +4,18 @@ import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import Field from './fields';
 import fieldReducer from './fieldReducer';
+import counterReducer from './counterReducer';
+import Counter from './counter';
 
 const reducers = combineReducers({
-    field: fieldReducer
+    field: fieldReducer,
+    counter: counterReducer
 });
 
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
         <Field initialValue='Teste' />
+        <Counter/>
     </Provider>,
     document.getElementById('root')
 );
